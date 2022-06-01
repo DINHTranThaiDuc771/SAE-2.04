fb53161a036ff1860d8d00da0d544b6d4b248219
Duc DINH <62024308+username1778@users.noreply.github.com>
2022-06-01 16:50:13 +0200

    Update r6.sql

--- phase1/r6.sql
+++ phase1/r6.sql
@@ -1,2 +1,8 @@
 /*Lister tous les clients (numéro et nom) ayant acheté des BD de la série ‘Astérix
 le gaulois’.*/
+Select c.numClient,nomClient
+From   Client c join Vente v on c.numClient = v.numClient
+                join Concerner c1 on c1.numVente = v.numVente 
+                join BD b on b.isbn = c1.isbn
+                join Serie s on s.numSerie = b.numSerie
+Where  nomSerie = 'Asterix le gaulois'
