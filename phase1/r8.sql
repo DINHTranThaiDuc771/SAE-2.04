e9c044f2b0d7a83c38115251f59ae12aa7647e7d
HirsuitCadet <59114320+HirsuitCadet@users.noreply.github.com>
2022-06-01 16:02:12 +0200

    done r8.sql

--- phase1/r8.sql
+++ phase1/r8.sql
@@ -1,3 +1,7 @@
+/*Construire et afficher une vue bdEditeur qui affiche le nombre de BD vendues
+par an et par éditeur, par ordre croissant des années et des noms d’éditeurs. On y
+affichera le nom de l’éditeur, l’année considérée et le nombre de BD publiées.*/
+
 create or replace view vueBdEditeur 
 as 
 select c.quantite,  v.dteVente, e.nomEditeur from Concerner c JOIN BD b
