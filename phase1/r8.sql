389862f2ee42719b32d0bffc9fc32897f72eac58
Duc DINH <62024308+username1778@users.noreply.github.com>
2022-06-01 16:45:34 +0200

    Update r8.sql

--- phase1/r8.sql
+++ phase1/r8.sql
@@ -2,10 +2,10 @@
 donc dépensé le plus d’argent en BD) en affichant leur numéro, nom et adresse
 mail, ainsi que le nombre total de BD qu’ils ont acheté (champ nbBD en tenant
 compte des quantités achetées), ainsi que le total de leurs achats (champ coutA).*/
-CREATE OR REPLACE view best5 AS  
-Select c.numClient, nomClient, mailClient, Sum(quantite) as "quantiteBD", Sum(prixVente) as "coutA" 
+CREATE OR REPLACE view best5(numClient,nomClient,mailClient,quantiteBD,coutA) AS  
+Select c.numClient, nomClient, mailClient, Sum(quantite) , Sum(prixVente*quantite) 
 From   Client c join Vente v      on v.numClient = c.numClient
                 join Concerner c1 on c1.numVente = v.numVente
 Group by c.numClient
-Order by Sum(prixVente) DESC
+Order by Sum(prixVente*quantite) DESC
 Limit 5;
