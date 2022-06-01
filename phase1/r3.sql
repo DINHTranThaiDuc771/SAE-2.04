1b5fab452e4014dbd9ef52626f6d0b97e39c9701
EnzoF76 <81768542+EnzoF76@users.noreply.github.com>
2022-06-01 16:54:56 +0200

    ajout commentaire r3
    enzo

--- phase1/r3.sql
+++ phase1/r3.sql
@@ -1,6 +1,7 @@
 /*Lister les clients (numéro, nom) avec leur coût total d’achats (que l’on nommera
 coutA) triés par leur coût décroissant, qui ont totalisé au moins 50000€
 d’achats...*/
+
 SELECT		cl.numClient, nomClient, SUM(prixVente*quantite) AS "coutA"
 FROM		Client cl JOIN Vente v ON cl.numClient = v.numClient
 		  	  JOIN Concerner c ON v.numVente = c.numVente
