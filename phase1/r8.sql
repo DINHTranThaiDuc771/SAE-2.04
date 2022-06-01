ff99e5966caaee54650fb167518c11ff52965526
HirsuitCadet <59114320+HirsuitCadet@users.noreply.github.com>
2022-06-01 15:59:47 +0200

    Update r8.sql

--- phase1/r8.sql
+++ phase1/r8.sql
@@ -1,1 +1,13 @@
+create or replace view vueBdEditeur 
+as 
+select c.quantite,  v.dteVente, e.nomEditeur from Concerner c JOIN BD b
+                                             ON c.isbn = b.isbn 
+                                             JOIN Serie s 
+                                             ON b.numSerie = s.numSerie 
+                                             JOIN Editeur e 
+                                             ON s.numEditeur = e.numEditeur
+                                             JOIN Vente v 
+                                             ON c.numVente = v.numV ente
+                                             order by dteVente, nomEditeur;
 
+select * from vuebdEditeur;  
