fcd06fbdc28581f0d7b959e4b6c7801409e01a4c
HirsuitCadet <59114320+HirsuitCadet@users.noreply.github.com>
2022-06-01 17:11:03 +0200

    Done r9.sql

--- phase1/r9.sql
+++ phase1/r9.sql
@@ -2,15 +2,12 @@
 par an et par éditeur, par ordre croissant des années et des noms d’éditeurs. On y
 affichera le nom de l’éditeur, l’année considérée et le nombre de BD publiées.*/
 
-create or replace view vueBdEditeur as 
-select c.quantite,  v.dteVente, e.nomEditeur from Concerner c JOIN BD b
-                                             ON c.isbn = b.isbn 
-                                             JOIN Serie s 
-                                             ON b.numSerie = s.numSerie 
-                                             JOIN Editeur e 
-                                             ON s.numEditeur = e.numEditeur
-                                             JOIN Vente v 
-                                             ON c.numVente = v.numV ente
-                                             order by dteVente, nomEditeur;
-
-select * from vuebdEditeur;  
+Create or replace view vueBdEditeur
+as
+Select nomEditeur as "Nom éditeur", EXTRACT(Year From dteVente) as "Année de vente", count(quantite) as "Nombre de BD vendues"
+From  Editeur e join Serie s on s.numEditeur = e.numEditeur
+                join BD b on b.numSerie = s.numSerie
+                join Concerner c1 on c1.isbn = b.isbn
+                join Vente v on c1.numVente = v.numVente
+Group by EXTRACT(Year from v.dteVente), nomEditeur
+Order by EXTRACT(Year from v.dteVente), nomEditeur;
