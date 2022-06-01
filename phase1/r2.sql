0cc54876107f06d6d0d1a8b8ec29ef238f26cdcc
EnzoF76 <81768542+EnzoF76@users.noreply.github.com>
2022-06-01 16:54:30 +0200

    ajout commentaire r2
    enzo

--- phase1/r2.sql
+++ phase1/r2.sql
@@ -1,3 +1,7 @@
+/* Lister les clients (numéro, nom) et leur nombre d’achats (que l’on nommera
+nbA) triés par ordre décroissant de leur nombre d’achats (sans prendre en compte
+la quantité achetée) */
+
 SELECT		cl.numClient, nomClient, SUM(quantite) AS "nbA"
 FROM		Client cl JOIN Vente v ON cl.numClient = v.numClient
 			            JOIN Concerner c ON v.numVente = c.numVente
