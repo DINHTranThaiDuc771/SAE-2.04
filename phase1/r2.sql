2b25243dfa271d791ca3c18cb96ab99d42885fdd
EnzoF76 <81768542+EnzoF76@users.noreply.github.com>
2022-06-01 15:44:09 +0200

    Update r2.sql

--- phase1/r2.sql
+++ phase1/r2.sql
@@ -1,1 +1,5 @@
-
+SELECT		cl.numClient, nomClient, SUM(quantite) AS "nbA"
+FROM		Client cl JOIN Vente v ON cl.numClient = v.numClient
+			            JOIN Concerner c ON v.numVente = c.numVente
+GROUP BY 	cl.numClient
+ORDER BY	SUM(quantite) DESC;
