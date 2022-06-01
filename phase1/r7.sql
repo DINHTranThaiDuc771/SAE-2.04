267a634cfca3d97bf7469070aff8172179b50582
DINH Tran Thai Duc <nguyentrunghieublabla@gmail.com>
2022-06-01 15:37:47 +0200

    r7 Done

--- phase1/r7.sql
+++ phase1/r7.sql
@@ -1,1 +1,21 @@
+--Lister les clients (numéro et nom) qui n’ont acheté que les BD de la série ‘Asterix
+--le gaulois’ (en utilisant la clause EXCEPT)
+--'Asterix le gaulois'
 
+
+
+Select c.numClient,nomClient
+From   Client c join Vente v on c.numClient = v.numClient
+                join Concerner c1 on c1.numVente = v.numVente 
+                join BD b on b.isbn = c1.isbn
+                join Serie s on s.numSerie = b.numSerie
+Where  nomSerie = 'Asterix le gaulois'
+
+Except 
+
+Select c.numClient,nomClient
+From   Client c join Vente v on c.numClient = v.numClient
+                join Concerner c1 on c1.numVente = v.numVente 
+                join BD b on b.isbn = c1.isbn
+                join Serie s on s.numSerie = b.numSerie
+Where  nomSerie != 'Asterix le gaulois';
