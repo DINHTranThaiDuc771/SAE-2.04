850cb49aaf22db0347207e81b33ae838e7ef94b5
HirsuitCadet <59114320+HirsuitCadet@users.noreply.github.com>
2022-06-01 16:48:44 +0200

    Done r10.sql

--- phase1/r10.sql
+++ phase1/r10.sql
@@ -1,1 +1,8 @@
-
+create or replace view vueBdEd10
+as
+select nomEditeur, mailEditeur, COUNT(isbn) as "nombre BD"
+from Editeur e join Serie s on e.numEditeur = s.numEditeur
+               join bd    b on s.numSerie   = b.numSerie
+Group by e.numEditeur
+Having count(isbn) >=10
+Order by e.numEditeur;
