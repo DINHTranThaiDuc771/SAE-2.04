c84e2185a40617aab179411b174bd548a8277058
HirsuitCadet <59114320+HirsuitCadet@users.noreply.github.com>
2022-06-01 17:12:56 +0200

    Done r10.sql

--- phase1/r10.sql
+++ phase1/r10.sql
@@ -1,3 +1,7 @@
+/*Construire et afficher une vue bdEd10 qui affiche les éditeurs qui ont publié plus
+de 10 BD, en donnant leur nom et email, ainsi que le nombre de BD différentes
+qu’ils ont publiées.*/
+
 create or replace view vueBdEd10 as
 select nomEditeur, mailEditeur, COUNT(isbn) as "nombre BD"
 from Editeur e join Serie s on e.numEditeur = s.numEditeur
