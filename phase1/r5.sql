e6cd5525b314e1fca8363846c2ebcece9685b37c
Kimaaaaaaa <102507341+Kimaaaaaaa@users.noreply.github.com>
2022-06-01 16:55:48 +0200

    update r5.sql

--- phase1/r5.sql
+++ phase1/r5.sql
@@ -1,1 +1,7 @@
+CREATE OR REPLACE VIEW CA AS
+SELECT SUM(prixVente*quantite) AS CA, EXTRACT(YEAR FROM dteVente) AS annee
+FROM Concerner c JOIN Vente v ON c.numVente = v.numVente
+GROUP BY annee
+ORDER BY annee; 
+
 
