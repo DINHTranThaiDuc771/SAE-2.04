dac3d5d3b26190d2b3b9efec4844cc8b55a71d21
EnzoF76 <81768542+EnzoF76@users.noreply.github.com>
2022-06-01 16:55:33 +0200

    ajout commentaire r4
    enzo

--- phase1/r4.sql
+++ phase1/r4.sql
@@ -1,3 +1,7 @@
+/* Afficher le chiffre d’affaire des ventes effectuées en 2021 (on pourra utiliser la
+fonction extract pour récupérer l’année seule d’une date) */
+
+
 SELECT SUM(prixVente*quantite)
 FROM Concerner c JOIN Vente v ON c.numVente = v.numVente
 WHERE EXTRACT(YEAR FROM dteVente) = 2021;
