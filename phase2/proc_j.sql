867d161a2c5079e640bd4e0849308d9a13fdaa34
Duc DINH <62024308+username1778@users.noreply.github.com>
2022-06-07 13:19:11 +0200

    Create proc_j.sql

---
+++ phase2/proc_j.sql
@@ -0,0 +1,10 @@
+/*On souhaite classer tous les clients par leur quantité totale d'achats de BD. Ainsi
+on veut associer à chaque client son rang de classement en tant qu'acheteur dans
+l'ordre décroissant des quantités achetées. Ainsi le client de rang 1 (classé
+premier) aura totalisé le plus grand nombre d'achats.
+Vous devez donc créer un nouveau type de données ‘rangClient’, qui associe
+l'identifiant du client, son nom et son classement dans les acheteurs (attribut
+nommé ‘rang’).
+Ecrire une fonction qui renvoie pour tous les clients, son identifiant, son nom et
+son classement d'acheteur décrit ci-dessus.
+NB : on pourra avantageusement utiliser une boucle FOR ou un curseur... */
