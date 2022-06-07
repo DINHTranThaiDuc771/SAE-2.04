d8ac2f8b58adfeecfb2c5d1e609a10748d90dc50
Duc DINH <62024308+username1778@users.noreply.github.com>
2022-06-07 13:01:23 +0200

    Update proc_h.sql
    First checked by Duc

--- phase2/proc_h.sql
+++ phase2/proc_h.sql
@@ -19,3 +19,38 @@ On rajoutera le comportement suivant : si le minimum est égal au maximum pour
 un client, on affichera le message 'Egalité du minimum et maximum pour le
 client %' en précisant le nom du client.
 NB : utiliser une boucle FOR ou un curseur...*/
+
+Drop type if exists clientBD cascade;
+Create type clientBD as (num int,nom varchar(11),mini int, maxi int, somme int);
+Create or replace function proc_h ()
+returns setOf clientBD
+As $$
+  Declare
+    tuple clientBD;
+    client Client%ROWTYPE;
+  Begin
+    for client in Select * from Client
+    Loop
+      tuple.num := client.numClient;
+      tuple.nom := client.nomClient;
+      Select min(quantite) into tuple.mini 
+      From Client c join Vente v on v.numClient = c.numClient
+                    join Concerner c1 on c1.numVente = v.numVente
+      Where c.numClient = client.numClient;
+      
+      Select max(quantite) into tuple.maxi 
+      From Client c join Vente v on v.numClient = c.numClient
+                    join Concerner c1 on c1.numVente = v.numVente
+      Where c.numClient = client.numClient;
+      
+      Select sum(quantite) into tuple.somme
+      From Client c join Vente v on v.numClient = c.numClient
+                    join Concerner c1 on c1.numVente = v.numVente
+      Where c.numClient = client.numClient;
+      return next tuple;
+    end loop;
+    return;
+  End
+$$Language PLpgSQL;
+--Test
+Select * from proc_h();
