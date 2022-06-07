a216b56688af7b8432b032733daf152936fab2f9
Duc DINH <62024308+username1778@users.noreply.github.com>
2022-06-07 13:13:46 +0200

    Update proc_i.sql
    First checked by Duc

--- phase2/proc_i.sql
+++ phase2/proc_i.sql
@@ -8,3 +8,21 @@ Pour gérer cela, on utilisera le code d'erreur FOREIGN_KEY_VIOLATION
 dans un bloc EXCEPTION dans lequel on supprimera tous les tuples de la table
 ‘Concerner’ qui possèdent ce numéro de vente, avant de supprimer la venet ellemême. On pourra au passage afficher aussi un message d'avertissement sur cette
 exception.*/
+Create or replace function proc_i (num_vente_param Vente.numVente%TYPE)
+returns void
+As $$
+  Begin
+    Perform * from Vente Where numVente = num_vente_param;
+    if not found then
+      raise exception 'Ce numero de vente n''existe pas';
+    end if;
+    Delete from Vente
+    Where numVente = num_vente_param;
+  Exception
+    When FOREIGN_KEY_VIOLATION then
+      Delete from Concerner
+      Where numVente = num_vente_param;
+  End
+$$Language PLpgSQL;
+--Test 
+Select * from proc_i(1);
