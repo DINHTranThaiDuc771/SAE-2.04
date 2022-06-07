c6204f372ac04ec95dfb723076084de41784f0e0
Duc DINH <62024308+username1778@users.noreply.github.com>
2022-06-07 10:56:41 +0200

    Create proc_h.sql

---
+++ phase2/proc_h.sql
@@ -0,0 +1,21 @@
+/*Ecrire une procédure qui renvoie pour tous les clients sa plus petite quantitéEcrire une procédure qui renvoie pour tous les clients sa plus petite quantité
+achetée (min) et sa plus grande quantité achetée (max) et la somme totale de ses
+quantités achetées de BD.
+Vous devrez donc créer un type composite ‘clientBD’ comportant quatre
+attributs: l'identifiant du client, son nom, sa plus petite quantité achetée, sa plus
+grande quantité achetée, et la somme totale de ses quantités achetées. Votre
+procédure devra retourner des éléments de ce type de données.
+On rajoutera le comportement suivant : si le minimum est égal au maximum pour
+un client, on affichera le message 'Egalité du minimum et maximum pour le
+client %' en précisant le nom du client.
+NB : utiliser une boucle FOR ou un curseur...
+achetée (min) et sa plus grande quantité achetée (max) et la somme totale de ses
+quantités achetées de BD.
+Vous devrez donc créer un type composite ‘clientBD’ comportant quatre
+attributs: l'identifiant du client, son nom, sa plus petite quantité achetée, sa plus
+grande quantité achetée, et la somme totale de ses quantités achetées. Votre
+procédure devra retourner des éléments de ce type de données.
+On rajoutera le comportement suivant : si le minimum est égal au maximum pour
+un client, on affichera le message 'Egalité du minimum et maximum pour le
+client %' en précisant le nom du client.
+NB : utiliser une boucle FOR ou un curseur...*/
