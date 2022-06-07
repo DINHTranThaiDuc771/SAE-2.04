cd59c74d164fe8b56c6cf903392e26395ca84e99
Duc DINH <62024308+username1778@users.noreply.github.com>
2022-06-07 10:56:09 +0200

    Create proc_g.sql

---
+++ phase2/proc_g.sql
@@ -0,0 +1,5 @@
+/*Écrire une procédure SQL utilisant un curseur, qui classe pour un éditeur dont le
+nom est donné en entrée, les clients de cet éditeur en trois catégories selon le
+nombre de BD qu’ils leur ont achetées : les « très bons clients » (plus de 10
+achats strictement), les « bons clients » (entre 2 et 10 BD), les « mauvais
+clients » (moins ou égal à 2 BD) */
