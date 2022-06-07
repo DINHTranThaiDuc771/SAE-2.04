622c288c61b8e8fa41293deb8604354f52ae2616
Duc DINH <62024308+username1778@users.noreply.github.com>
2022-06-07 16:24:59 +0200

    Update proc_e.sql

--- phase2/proc_e.sql
+++ phase2/proc_e.sql
@@ -1,1 +1,18 @@
-
+/*Créer une fonction qui prend en paramètre un nombre nbBD de BD et une année
+donnée, et qui renvoie la liste des éditeurs ayant vendu au moins ce nombre de
+BD dans l’année en question. Si aucun éditeur ne répond à la requête, le signaler
+par un message approprié. */
+Create or replace function proc_e (nbBD int, annee float)
+returns setOf Editeur%ROWTYPE
+AS $$
+  Begin
+  Return query Select e.*
+               From Vente v join Concerner c1 on c1.numVente = v.numVente
+                            join BD b on b.isbn = c1.isbn
+                            join Serie s on s.numSerie = b.numSerie
+                            join Editeur e on e.numEditeur = s.numEditeur
+               Where Extract(YEAR from dteVente) = annee 
+               Group by numEditeur
+               Having sum(quantite) > nbBD;
+  End
+$$Language PLpgSQL;
