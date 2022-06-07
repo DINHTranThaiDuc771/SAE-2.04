f687b336c6724290368bc326567dd94e8f8e8053
Duc DINH <62024308+username1778@users.noreply.github.com>
2022-06-07 14:37:10 +0200

    Faut corriger après

--- phase2/proc_g.sql
+++ phase2/proc_g.sql
@@ -1,8 +1,9 @@
-/*Écrire une procédure SQL utilisant un curseur, qui classe pour un éditeur dont le
+/*Écrire une fonction SQL utilisant un curseur, qui classe pour un éditeur dont le
 nom est donné en entrée, les clients de cet éditeur en trois catégories selon le
-nombre de BD qu’ils leur ont achetées : les « très bons clients » (plus de 10
-achats strictement), les « bons clients » (entre 2 et 10 BD), les « mauvais
-clients » (moins ou égal à 2 BD) */
+nombre de BD qu’ils leur ont achetées (attention on ne comptabilisera pas les
+quantités achetées, seulement le nombre de BD distinctes) : les « très bons
+clients » (plus de 10 achats strictement), les « bons clients » (entre 2 et 10 BD),
+les « mauvais clients » (moins ou égal à 2 BD) */
 
 Drop type if exists type_proc_g cascade;
 Create type type_proc_g as (nom_client varchar(11), type text);
