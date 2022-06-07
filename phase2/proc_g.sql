294017b1b40a0499f8e9aff5dd68860bc9f36651
Duc DINH <62024308+username1778@users.noreply.github.com>
2022-06-07 11:31:54 +0200

    Update proc_g.sql

--- phase2/proc_g.sql
+++ phase2/proc_g.sql
@@ -10,43 +10,43 @@ Create type type_proc_g as (nom_client varchar(11), type text);
 Create or replace function proc_g (nom_editeur_param Editeur.nomEditeur%TYPE)
 returns setOf type_proc_g
 As $$
-	Declare
-		cli Client;
-		curseur CURSOR FOR select * from Client;
-    achat bigint;
-    tuple type_proc_g;
-	Begin
-		Drop view if exists vue_proc_g cascade;
-    Create view vue_proc_g as
-    Select nomEditeur, c.numClient, Sum (quantite) as "quantite_achat"
-    From   Editeur e join Serie s on s.numEditeur = e.numEditeur
+Declare
+	cli Client;
+	curseur CURSOR FOR select * from Client;
+	achat bigint;
+	tuple type_proc_g;
+Begin
+	Drop view if exists vue_proc_g cascade;
+	Create view vue_proc_g as
+	Select nomEditeur, c.numClient, nomClient,Sum (quantite) as "quantite_achat"
+	From   Editeur e join Serie s on s.numEditeur = e.numEditeur
                      join BD    b on b.numSerie   = s.numSerie
                      join Concerner c1 on c1.isbn = b.isbn
                      join Vente v      on v.numVente = c1.numVente
                      join Client c     on c.numClient= v.numClient
-    Group by c.numClient,nomEditeur
-    Order by nomEditeur;
+	Group by c.numClient,nomEditeur
+	Order by nomEditeur;
     
     Open curseur;
-		Loop
-			Fetch curseur into cli;
-			Exit when not found;
-      tuple.nom_client := cli.nomClient;
-      Select quantite_achat into achat 
-      from vue_proc_g 
-      where nomEditeur = nom_editeur_param and numClient = cli.numClient;
-      if achat >10 then 
-        tuple.type := 'très bons clients';
-      end if;
-      if achat <=10 and achat > 2 then 
-        tuple.type := 'bons clients';
-      end if;
-      if achat<=2 then 
-        tuple.type := 'mauvais clients';
-      end if;
-      achat :=0;
-      return next tuple;
-		end loop;
-		close curseur;
-	End
+      Loop
+	      Fetch curseur into cli;
+	      Exit when not found;
+	      tuple.nom_client := cli.nomClient;
+	      Select quantite_achat into achat 
+	      from vue_proc_g 
+	      where nomEditeur = nom_editeur_param and numClient = cli.numClient;
+	      if achat >10 then 
+		tuple.type := 'très bons clients';
+	      end if;
+	      if achat <=10 and achat > 2 then 
+		tuple.type := 'bons clients';
+	      end if;
+	      if achat<=2 then 
+		tuple.type := 'mauvais clients';
+	      end if;
+	      achat :=0;
+	      return next tuple;
+      end loop;
+   close curseur;
+End
 $$Language PLpgSQL;
