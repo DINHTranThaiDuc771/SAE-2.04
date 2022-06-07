6023ed8893d15ad3b07c6e62aec6c6ee99e3ca0d
Duc DINH <62024308+username1778@users.noreply.github.com>
2022-06-07 13:51:05 +0200

    Create proc_d.sql

---
+++ phase2/proc_d.sql
@@ -0,0 +1,6 @@
+/*Créer une fonction stockée qui prend en paramètre le nom d’une série de BD et
+qui renvoie les clients ayants acheté tous les albums de la série (utiliser des
+boucles FOR et/ou des curseurs).
+Si aucun client ne répond à la requête alors on affichera un message
+d’avertissement ‘Aucun client n’a acheté tous les exemplaires de la série %’, en
+complétant le ‘ %’ par le nom de la série.*/
