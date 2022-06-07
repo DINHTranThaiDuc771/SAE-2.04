f1d0f889718cddee948eab55afbc1ec58ea178d7
Duc DINH <62024308+username1778@users.noreply.github.com>
2022-06-07 13:04:39 +0200

    Create proc_i.sql

---
+++ phase2/proc_i.sql
@@ -0,0 +1,10 @@
+/*Ecrire une fonction qui supprime une vente dont l'identifiant est passé en
+paramètre.
+Vérifier d'abord que la vente associée à l'identifiant existe, si elle n'existe pas
+afficher un message d'erreur le mentionnant; si elle existe on la supprime.
+Cette suppression va générer une violation de clé étrangère dans la table
+‘Concerner’.
+Pour gérer cela, on utilisera le code d'erreur FOREIGN_KEY_VIOLATION
+dans un bloc EXCEPTION dans lequel on supprimera tous les tuples de la table
+‘Concerner’ qui possèdent ce numéro de vente, avant de supprimer la venet ellemême. On pourra au passage afficher aussi un message d'avertissement sur cette
+exception.*/
