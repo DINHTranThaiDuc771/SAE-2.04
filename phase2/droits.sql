87b2ed889915e8c3739775e38009d1862f011af4
EnzoF76 <81768542+EnzoF76@users.noreply.github.com>
2022-06-07 16:31:28 +0200

    droits upload 1
    nz

---
+++ phase2/droits.sql
@@ -0,0 +1,15 @@
+/* ADMINISTRATEUR */
+GRANT   * 
+ON      Serie, BD, Editeur, Auteur, Vente, Client, Concerner 
+TO      administrateur 
+WITH GRANT OPTION;
+
+/* VENDEUR */
+GRANT   SELECT, INSERT 
+ON      Vente, Client, Concerner
+TO      vendeur;
+
+/* EDITEUR */
+GRANT   SELECT, INSERT 
+ON      Vente, Client, Concerner, BD, Serie, Auteur
+TO      editeur; 
