e1f5f182b7c2c82e37d40e7b146cea8615c40613
HirsuitCadet <59114320+HirsuitCadet@users.noreply.github.com>
2022-06-08 11:18:43 +0200

    Done proc_i.sql

--- phase2/proc_i.sql
+++ phase2/proc_i.sql
@@ -8,6 +8,7 @@ Pour gérer cela, on utilisera le code d'erreur FOREIGN_KEY_VIOLATION
 dans un bloc EXCEPTION dans lequel on supprimera tous les tuples de la table
 ‘Concerner’ qui possèdent ce numéro de vente, avant de supprimer la venet ellemême. On pourra au passage afficher aussi un message d'avertissement sur cette
 exception*/
+
 Create or replace function proc_i (num_vente_param Vente.numVente%TYPE)
 returns void
 As $$
